92ed8aeffd2a2c278a1d637d4b787deb44e9cfa6
ramachandran-varadaraju <rajram92@hotmail.com>
2021-06-11 08:14:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let countDate = new Date("jul 4, 2021 00:00:00").getTime();
+
+const countDown = () => {
+  let now = new Date().getTime();
+
+  let gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+};
+
+setInterval(() => {
+  countDown();
+}, 1000);
